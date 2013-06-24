355d271d8afe14f7838cef272683f64e84ba51be
Peter Vandenberk <pvandenberk@mac.com>
2013-06-24 19:23:53 +0100

    exclusion rule for .gitkeep

--- lib/git-conform/checkers/file_not_empty_checker.rb
+++ lib/git-conform/checkers/file_not_empty_checker.rb
@@ -2,6 +2,9 @@ module Git
   module Conform
     class FileNotEmptyChecker < FileChecker
 
+      @file_exclusion_patterns << '.gitkeep'
+      # .gitkeep is (by convention) an empty file
+
       def conforms?
         super && !File.size?(@filename).nil?
       end
---
+++ spec/lib/git-conform/checkers/file_not_empty_checker_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Git::Conform::FileNotEmptyChecker do
+
+  it "excludes '.gitkeep' by default" do
+    expect(described_class.file_exclusion_patterns).to include '.gitkeep'
+  end
+
+end

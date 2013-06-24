2978f298f68623b4879626ba7ca8291fcee14a89
Peter Vandenberk <pvandenberk@mac.com>
2013-06-24 09:02:55 +0100

    Non-lowercase files found in typical Ruby project

--- lib/git-conform/checkers/lowercase_filename_checker.rb
+++ lib/git-conform/checkers/lowercase_filename_checker.rb
@@ -2,6 +2,16 @@ module Git
   module Conform
     class LowercaseFilenameChecker < FileChecker
 
+      @file_exclusion_patterns << '*Brewfile*'
+      @file_exclusion_patterns << '*Rakefile*'
+      @file_exclusion_patterns << '*Gemfile*'
+      @file_exclusion_patterns << '*Guardfile*'
+      @file_exclusion_patterns << '*Capfile*'
+      @file_exclusion_patterns << '*Procfile*'
+      @file_exclusion_patterns << '*Vagrantfile*'
+
+      @file_exclusion_patterns << '*README*'
+
       def conforms?
         super && @filename.match(/[A-Z]/).nil?
       end

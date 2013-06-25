f956597edce5b4718f3553c585f475fee30115fc
Peter Vandenberk <pvandenberk@mac.com>
2013-06-25 09:06:17 +0100

    also ignore .gitmodules

--- lib/git-conform/checkers/tab_character_checker.rb
+++ lib/git-conform/checkers/tab_character_checker.rb
@@ -6,6 +6,9 @@ module Git
       # .gitconform is designed to be maintained using the 'git config'
       # command, which uses tab characters for its indentation purposes
 
+      @file_exclusion_patterns << '.gitmodules'
+      # .gitmodules uses a format very similar to the 'git config' one!
+
       def conforms?
         super && !content.include?("\t")
       end
--- spec/lib/git-conform/checkers/tab_character_checker_spec.rb
+++ spec/lib/git-conform/checkers/tab_character_checker_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Git::Conform::TabCharacterChecker do
 
+  it "excludes '.gitmodules' by default" do
+    expect(described_class.file_exclusion_patterns).to include '.gitmodules'
+  end
+
   it "excludes '.gitconform' by default" do
     expect(described_class.file_exclusion_patterns).to include '.gitconform'
   end
